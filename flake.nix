ac7513f11e2f91bd927258a5f7d13d59973555ae
krosh <krosh@kroshhaker.dev>
2026-08-30 17:07:57 +0300

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "Backend development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          node = pkgs.nodejs_24;
+
+          pnpm = pkgs.pnpm_10.override {
+            nodejs = node;
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              node
+              pnpm
+              pkgs.docker
+            ];
+
+            shellHook = ''
+              echo "Node: $(node --version)"
+              echo "pnpm: $(pnpm --version)"
+              echo "Docker: $(docker --version)"
+            '';
+          };
+        }
+      );
+    };
+}
